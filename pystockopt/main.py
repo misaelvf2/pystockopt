5da1acd4eefc1f219a0d6e9f46b4becdd9d3273f
Misael Valentin <misael.vf2@gmail.com>
2021-10-02 23:36:03 -0400

    Factored API calls out of table rows

--- pystockopt/main.py
+++ pystockopt/main.py
@@ -33,46 +33,56 @@ def generate_table() -> Table:
     table = Table(show_header=True, header_style="bold cyan")
 
     table.add_column("Contract")
+    table.add_column("Ticker")
     table.add_column("Last Price")
     table.add_column("% Change")
-    table.add_column("Ticker")
     table.add_column("Type")
     table.add_column("Premium")
     table.add_column("Strike")
     table.add_column("Expiration")
 
+    last_price = my_option.last_price
+    percent_change = my_option.percent_change
+
     table.add_row(
         my_option.symbol,
-        str(my_option.last_price),
-        (f"[bold green]{my_option.percent_change}"
-         if my_option.percent_change >= 0
-         else f"[bold red]{my_option.percent_change}"),
         my_option.ticker,
+        str(last_price),
+        (f"[bold green]{percent_change}"
+         if my_option.percent_change >= 0
+         else f"[bold red]{percent_change}"),
         my_option.opt_type.capitalize(),
         str(my_option.premium),
         str(my_option.strike),
         str(my_option.expiration)
     )
+
     return table
 
 
 def main():
     with Live(generate_table(), refresh_per_second=4) as live:
-        for _ in range(40):
+        for _ in range(5):
             time.sleep(0.4)
             live.update(generate_table())
 
 
 def test():
     my_session = requests_cache.CachedSession('yfinance.cache')
-    my_option = Option(ticker="PLTR", opt_type="call", premium=3.59,
-                       strike=25.00, expiration=date(2022, 1, 21),
-                       _session=my_session)
+    # my_option = Option(ticker="PLTR", opt_type="call", premium=3.59,
+    #                    strike=25.00, expiration=date(2022, 1, 21),
+    #                    _session=my_session)
 
-    inspect(Option.get_options_chain_from_yf(
-        my_option.stock, my_option.expiration), methods=True)
+    # inspect(Option.get_options_chain_from_yf(
+    #     my_option.stock, my_option.expiration), methods=True)
+    grid = Table.grid(expand=True)
+    grid.add_column()
+    grid.add_column(justify="right")
+    grid.add_row("Raising shields", "[bold magenta]COMPLETED [green]:heavy_check_mark:")
+
+    print(grid)
 
 
 if __name__ == "__main__":
-    main()
-    # test()
+    # main()
+    test()
