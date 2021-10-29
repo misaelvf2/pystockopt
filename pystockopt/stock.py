848eb370e2f0327c051c8b4dd24c435f6bb010d6
Misael Valentin <misael.vf2@gmail.com>
2021-10-28 21:58:54 -0400

    Added percent_change method; refactored some stuff

--- pystockopt/stock.py
+++ pystockopt/stock.py
@@ -1,22 +1,32 @@
 import yfinance as yf
+from pystockopt.utils import get_ticker_from_yf
 from pystockopt.security import Security
 
 
 class Stock(Security):
-    def __init__(self, ticker, _session):
+    def __init__(self, ticker, price, _session):
         self.ticker = ticker
-        self._stock = yf.Ticker(ticker, session=_session)
-        self.purchase_price = self._stock.info['regularMarketPrice']
+        self._stock = get_ticker_from_yf(ticker, _session)
+        self.purchase_price = price
 
     @property
-    def current_price(self):
+    def last_price(self):
         return self._stock.info['regularMarketPrice']
+
+    @property
+    def percent_change(self):
+        previous_close = self._stock.info['previousClose']
+        percent_change = (self.last_price - previous_close) / previous_close * 100
+        return percent_change
 
     @property
     def stock(self):
         return self._stock
 
+    @property
+    def company(self):
+        return self._stock.info['shortName']
+
     def __repr__(self):
         return f"<ticker: {self.ticker}, \
-             purchase price: {self.purchase_price}, \
-             current price: {self.current_price}>"
+             current price: {self.last_price}>"
