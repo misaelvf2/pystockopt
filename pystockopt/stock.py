78ad69e13f20d36840c355ddfe1e4f13500a6815
Misael Valentin <misael.vf2@gmail.com>
2021-11-05 23:43:32 -0400

    Fixes naming for price range methods Naming scheme for "price_range" methods did not follow convention.

--- pystockopt/stock.py
+++ pystockopt/stock.py
@@ -1,7 +1,10 @@
-import yfinance as yf
+"""
+Contains classes for representing and manipulating stocks
+"""
+
+import datetime
 from pystockopt.utils import get_ticker_from_yf
 from pystockopt.security import Security
-from datetime import date
 
 
 class Stock(Security):
@@ -15,6 +18,12 @@ class Stock(Security):
         return self._stock.info['regularMarketPrice']
 
     @property
+    def change(self):
+        previous_close = self._stock.info['previousClose']
+        change = self.last_price - previous_close
+        return change
+
+    @property
     def percent_change(self):
         previous_close = self._stock.info['previousClose']
         percent_change = (self.last_price - previous_close) / \
@@ -23,44 +32,75 @@ class Stock(Security):
 
     @property
     def stock(self):
+        """Reference to underlying stock ticker from yf."""
         return self._stock
 
     @property
     def company(self):
+        """Returns the stock's company name."""
         return self._stock.info['shortName']
 
-    def price_range(self, start, end):
+    def price_change(self, start, end):
+        """Returns change in price from given start and end dates."""
         start_price = self.price_on(start)
-        end_price = self.price_on(end)
+        end_price = self.price_on(end.replace(day=end.day - 1))
         return end_price - start_price
 
     def price_on(self, date):
+        """Returns price on given date."""
         price_history = self.stock.history(start=date)
         price_on_date = price_history.loc[str(date)]["Close"]
         return price_on_date
 
-    def max_price_range(self, start, end):
+    def price_range_values(self, start, end):
+        """Returns tuple consisting of the low and high values of the price range for a given time period."""
+        price_history = self.stock.history(start=start, end=end)
+        max_price = price_history["Close"].max()
+        min_price = price_history["Close"].min()
+        return (min_price, max_price)
+
+    def price_range(self, start, end):
+        """Returns the price range for given time period."""
         price_history = self.stock.history(start=start, end=end)
         max_price = price_history["Close"].max()
         min_price = price_history["Close"].min()
         return max_price - min_price
 
-    def max_price_range_percent(self, start, end):
+    def price_range_percent(self, start, end):
+        """Returns price range for given time period as percentage."""
         price_history = self.stock.history(start=start, end=end)
         max_price = price_history["Close"].max()
         min_price = price_history["Close"].min()
         return 100 * (max_price - min_price) / ((max_price + min_price) / 2)
 
+    def save_price_history(self, start, end):
+        """Saves price history of stock for given time period."""
+        price_history = self.stock.history(start=start, end=end)
+        price_history.to_csv(f"{self.ticker}_price_history_{start}.csv")
+
     def __repr__(self):
+        """String representation of Stock object."""
         return f"<ticker: {self.ticker}, \
              current price: {self.last_price}>"
 
 
 if __name__ == "__main__":
-    my_stock = Stock(ticker="PLTR")
+    my_stock = Stock(ticker="MSFT")
+    price_change = my_stock.price_change(
+        start=datetime.date(2021, 10, 5), end=datetime.date(2021, 11, 5)
+    )
+
     price_range = my_stock.price_range(
-        start=date(2021, 4, 28), end=date(2021, 10, 28))
-    max_price_range = my_stock.max_price_range(
-        start=date(2021, 4, 1), end=date(2021, 10, 28))
+        start=datetime.date(2021, 10, 5), end=datetime.date(2021, 11, 5)
+    )
+
+    price_range_values = my_stock.price_range_values(
+        start=datetime.date(2021, 10, 5), end=datetime.date(2021, 11, 5)
+    )
+
+    print(f"Price change: {price_change}")
     print(f"Price range: {price_range}")
-    print(f"Max price range: {max_price_range}")
+    print(f"Price range values: {price_range_values}")
+
+    my_stock.save_price_history(start=datetime.date(
+        2021, 10, 5), end=datetime.date(2021, 11, 5))
--- pystockopt/test_stocks.py
+++ pystockopt/test_stocks.py
@@ -25,11 +25,21 @@ class TestStocks(unittest.TestCase):
         result = self.my_stock.price_on(date(2021, 9, 29))
         self.assertAlmostEqual(result, 284.00, places=2)
 
+    def test_price_change(self):
+        result = self.my_stock.price_change(
+            start=date(2021, 1, 4), end=date(2021, 9, 29))
+        self.assertAlmostEqual(result, 67.24, places=2)
+
     def test_price_range(self):
         result = self.my_stock.price_range(
-            start=date(2021, 1, 4), end=date(2021, 9, 29))
-        print(result)
-        self.assertAlmostEqual(result, 67.72, places=2)
+            start=date(2021, 10, 5), end=date(2021, 11, 5))
+        self.assertAlmostEqual(result, 47.68, places=2)
+
+    def test_price_range_percent(self):
+        result = self.my_stock.price_range_percent(
+            start=date(2021, 10, 5), end=date(2021, 11, 5)
+        )
+        self.assertAlmostEqual(result, 15.25, places=2)
 
     if __name__ == "__main__":
         unittest.main()
